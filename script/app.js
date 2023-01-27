44f0b9eacdb98b468e52309952828054cf5a56ee
JackS1832 <jacksandison11@gmail.com>
2023-01-27 17:55:26 +0000

    Update app.js
    added an area function

--- script/app.js
+++ script/app.js
@@ -4,12 +4,17 @@ app.controller('myCtl', ['$scope', '$http', '$mdToast', '$mdDialog', '$sce', '$l
     function ($scope, $http, $mdToast, $mdDialog, $sce, $location,) {
         $scope.user = {
             name:''// this gets bound to the text box.
-
         };
         $scope.room = {
             width:null,
             height:null,
             area:null
+        };
+        
+        $scope.calculate = function(){
+          /* update the area of the room based upon width and height. */
+          $scope.room.area = $scope.room.width * $scope.room.height;
+          console.log($scope.room);
         }
     }
 ]);
