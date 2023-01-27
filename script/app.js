d900167622d2eacb45e96ccefdf02f6070dd9625
Andrew Allsopp <arallsopp@gmail.com>
2023-01-27 17:29:08 +0000

    added width and height capture

--- script/app.js
+++ script/app.js
@@ -3,7 +3,12 @@ var app = angular.module('myApp', ['ngSanitize', 'ngRoute', 'ngMaterial']);
 app.controller('myCtl', ['$scope', '$http', '$mdToast', '$mdDialog', '$sce', '$location',
     function ($scope, $http, $mdToast, $mdDialog, $sce, $location,) {
         $scope.user = {
-            name:'' // this gets bound to the text box.
+            name:''// this gets bound to the text box.
+
+        };
+        $scope.room = {
+            width:null,
+            height:null
         }
     }
 ]);
